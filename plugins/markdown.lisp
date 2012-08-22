221ea46288e89b23c11699ae21b5521fb4631bf0
Brit Butler <redline6561@gmail.com>
2012-08-22 13:43:17 -0400

    Add Markdown plugin with 3mbd's code highlighting support.

--- plugins/markdown.lisp
+++ plugins/markdown.lisp
@@ -1,9 +1,12 @@
 (eval-when (:compile-toplevel)
-  (ql:quickload '3mbd))
+  (ql:quickload '(3bmd 3bmd-ext-code-blocks)))
 
 (defpackage :coleslaw-md
   (:use :cl :coleslaw))
 
 (in-package :coleslaw-md)
 
-
+(defmethod render-content (text (format (eql :md)))
+  (let ((3mbd-code-blocks:*code-blocks* t))
+    (with-output-to-string (str)
+      (3bmd:parse-string-and-print-to-stream text str))))
