248614e516906e9dd745b1488a421f7a9f21bd4f
Brit Butler <redline6561@gmail.com>
2012-09-12 13:40:31 -0400

    Minor package tweak.

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,10 +1,11 @@
 (defpackage :coleslaw
   (:documentation "Homepage: <a href=\"http://github.com/redline6561/coleslaw\">Github</a>")
-  (:use :cl :closure-template)
+  (:use :cl)
   (:import-from :alexandria #:hash-table-values
                             #:make-keyword)
+  (:import-from :closure-template #:compile-template)
   (:export #:main
            #:add-injection
            #:render-content
-           #:render-feed
+           #:render
            #:deploy))
