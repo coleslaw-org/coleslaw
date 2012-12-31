04d9d523d82ebb4ca4889321f8a24a5aafdc1ef3
Ralph Moritz <ralph.moeritz@outlook.com>
2012-12-31 10:03:41 +0200

    Multi-site support

--- src/config.lisp
+++ src/config.lisp
@@ -13,6 +13,9 @@
    (title :initarg :title :initform "" :accessor title)
    (theme :initarg :theme :initform "hyde" :accessor theme)))
 
+(define-condition unknown-config-section-error (error)
+  ((text :initarg :text :reader text)))
+
 (defparameter *config* nil
   "A variable to store the blog configuration and plugin settings.")
 
@@ -35,8 +38,22 @@ are in the plugins folder in coleslaw's source directory."
                 (apply 'enable-plugin (plugin-path name) args)))
         (symbol (enable-plugin (plugin-path plugin)))))))
 
-(defun load-config (&optional (dir (user-homedir-pathname)))
-  "Load the coleslaw configuration from DIR/.coleslawrc. DIR is ~ by default."
+(defun load-config (config-key &optional (dir (user-homedir-pathname)))
+  "Load the coleslaw configuration for CONFIG-KEY from DIR/.coleslawrc. DIR is ~ by default."
   (with-open-file (in (merge-pathnames ".coleslawrc" dir))
-    (setf *config* (apply #'make-instance 'blog (read in))))
-  (load-plugins (plugins *config*)))
+    (let ((config-form (read in)))
+      (if (symbolp (car config-form))
+          ;; Single site config: ignore CONFIG-KEY.
+          (setf *config* (apply #'make-instance 'blog config-form))
+          ;; Multi-site config: load config section for CONFIG-KEY.
+          (let* ((config-key-pathname (cl-fad:pathname-as-directory config-key))
+                 (section (assoc config-key-pathname config-form
+                                 :key #'(lambda (str) (cl-fad:pathname-as-directory str))
+                                 :test #'equal)))
+            (if section
+                (progn 
+                  (setf *config* (apply #'make-instance 'blog (cdr section)))
+                  (setf (slot-value *config* 'repo) config-key))
+                (error 'unknown-config-section-error 
+                       :text (format nil "In ~A: No such key: '~A'." in config-key)))))
+      (load-plugins (plugins *config*)))))
