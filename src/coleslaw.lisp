6396e136d32f1527768853d59377d9f062003cda
Do Nhat Minh <mrordinaire@gmail.com>
2013-04-19 00:06:28 +0800

    first pass in adding github integration

--- src/coleslaw.lisp
+++ src/coleslaw.lisp
@@ -63,6 +63,15 @@ Additional args to render CONTENT can be passed via RENDER-ARGS."
            (curr (rel-path dest ".curr")))
       (ensure-directories-exist new-build)
       (run-program "mv ~a ~a" staging new-build)
+      (when (github *config*)
+        (let ((cname-filename (rel-path "" "~a/CNAME" new-build))
+              (stripped-url (subseq (domain *config*)
+                                    (+ 2 (position #\/ (domain *config*))))))
+          (format t "~a" cname-filename)
+          (with-open-file (cname cname-filename
+                                 :direction :output
+                                 :if-exists :supersede)
+                          (format cname "~a~%" stripped-url))))
       (when (probe-file prev)
         (delete-directory-and-files (truename prev) :if-does-not-exist :ignore))
       (when (probe-file curr)
