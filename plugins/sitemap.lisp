c2c45356d2dee8fb2e94bac84751256fa727dc90
Brit Butler <redline6561@gmail.com>
2013-04-29 16:59:25 -0400

    Update TODO. Minor sitemap plugin cleanup.

--- plugins/sitemap.lisp
+++ plugins/sitemap.lisp
@@ -19,7 +19,7 @@
   (let ((urls (append '("" "sitemap.xml") ; empty string is for root url
                       (mapcar #'page-url (find-all 'coleslaw:post)))))
     (write-page (rel-path (staging-dir *config*) "sitemap.xml")
-                (funcall (theme-fn :sitemap "feeds")
+                (funcall (theme-fn 'sitemap "feeds")
                          (list :config *config*
                                :urls urls
                                :pubdate (make-pubdate))))))
