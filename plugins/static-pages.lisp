cba6776afd999a8f5291ea6c4fbb93e1a99ae833
Brit Butler <redline6561@gmail.com>
2014-04-28 13:42:44 -0400

    Cleanup imports and use write-document.

--- plugins/static-pages.lisp
+++ plugins/static-pages.lisp
@@ -1,24 +1,26 @@
 (defpackage :coleslaw-static-pages
   (:use :cl)
   (:export #:enable)
-  (:import-from :coleslaw #:content
+  (:import-from :coleslaw #:*config*
+                          #:content
                           #:page-url
                           #:find-all
                           #:render
-                          #:publish))
+                          #:publish
+                          #:write-document))
 
 (in-package :coleslaw-static-pages)
 
 (defclass page (content)
-  ((:url :initarg :url :reader page-url)))
+  ((url :initarg :url :reader page-url)))
 
-(defmethod render ((object page))
+(defmethod render ((object page) &key next prev)
   ;; For now, we'll re-use the normal post theme.
   (funcall (theme-fn 'post) (list :config *config*
                                   :post object)))
 
 (defmethod publish ((doc-type (eql (find-class 'page))))
   (dolist (page (find-all 'page))
-    (write-file (page-path page) (render-page page nil))))
+    (write-document page)))
 
 (defun enable ())
