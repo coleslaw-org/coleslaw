3264848cde1d31d2443c1011dbf12120027bb2af
Brit Butler <redline6561@gmail.com>
2014-05-08 11:37:10 -0400

    Cleanup sitemap plugin a bit.

--- plugins/sitemap.lisp
+++ plugins/sitemap.lisp
@@ -1,28 +1,26 @@
 (defpackage :coleslaw-sitemap
   (:use :cl)
   (:import-from :coleslaw #:*config*
+                          #:index
                           #:deploy
                           #:page-url
-                          #:rel-path
-                          #:staging-dir
                           #:theme-fn
-                          #:write-file)
-  (:import-from :local-time #:format-timestring
-                            #:now)
+                          #:write-document)
   (:import-from :alexandria #:hash-table-values)
   (:export #:enable))
 
 (in-package :coleslaw-sitemap)
 
+(defclass sitemap (index)
+  ((urls :initarg :urls :reader urls)))
+
+(defmethod page-url ((object sitemap)) "sitemap.xml")
+
 (defmethod deploy :before (staging)
   "Render sitemap.xml under document root."
   (declare (ignore staging))
-  (let ((urls (append '("" "sitemap.xml") ; empty string is for root url
-                      (mapcar #'page-url (hash-table-values coleslaw::*site*)))))
-    (write-file (rel-path (staging-dir *config*) "sitemap.xml")
-                (funcall (theme-fn 'sitemap "sitemap")
-                         (list :config *config*
-                               :urls urls
-                               :pubdate (format-timestring nil (now)))))))
+  (let* ((urls (mapcar #'page-url (hash-table-values coleslaw::*site*)))
+         (sitemap (make-instance 'sitemap :urls (append '("" "sitemap.xml") urls))))
+    (write-document sitemap (theme-fn 'sitemap "sitemap"))))
 
 (defun enable ())
