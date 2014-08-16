d857acfeee664e5d70e6799086bfe4650c804137
Javier Olaechea <pirata@gmail.com>
2014-08-15 20:18:40 -0500

    Bind content-text & add quotes to HTML attributes

--- plugins/twitter-summary-card.lisp
+++ plugins/twitter-summary-card.lisp
@@ -1,5 +1,5 @@
 (defpackage :coleslaw-twitter-summary-card
-  (:use :cl)
+  (:use :cl :coleslaw)
   (:export #:enable))
 
 (in-package :coleslaw-twitter-summary-card)
@@ -7,12 +7,15 @@
 (defun summary-card (post twitter-handle)
   "TODO: Figure if and how to include twitter:url meta property."
   (format nil "<meta property=\"twitter:card\" content=\"summary\" />
-~@[<meta property=\"twitter:author\" content=~A />~]
-<meta property=\"twitter:title\" content=~A />
-<meta property=\"twitter:description\" content=~A />"
+~@[<meta property=\"twitter:author\" content=\"~A\" />~]
+<meta property=\"twitter:title\" content=\"~A\" />
+<meta property=\"twitter:description\" content=\"~A\" />"
           twitter-handle
           (title-of post)
-          (content-text post)))
+          (let ((text (content-text post)))
+            (if (< 200 (length text))
+                (subseq text 0 199)
+                text))))
 
 (defun enable (&key twitter-handle)
   (add-injection
