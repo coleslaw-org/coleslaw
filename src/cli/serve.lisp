171816c6cd47db090de92845c49a3343ea5f8ae4
Javier Olaechea <pirata@gmail.com>
2015-06-20 05:28:50 -0500

    (cli/serve.lisp)
      - Import functions used from the coleslaw package
      - Make sure that the app doesn't return immediately

--- src/cli/serve.lisp
+++ src/cli/serve.lisp
@@ -1,5 +1,10 @@
 (defpackage #:coleslaw-cli/serve
   (:use #:cl)
+  (:import-from #:coleslaw
+                #:*config*
+                #:discover-config-path
+                #:load-config
+                #:staging-dir)
   (:documentation "Serve the rendered blog.")
   (:export
    #:serve))
@@ -19,7 +24,11 @@
                     :port 4242 ))
     (push (hunchentoot:create-folder-dispatcher-and-handler "/"
                                                             (staging-dir *config*))
-          hunchentoot:*dispatch-table*)))
+          hunchentoot:*dispatch-table*)
+    ;; To prevent the app to return immediately. Should investigate further.
+    ;; See:
+    ;; http://stackoverflow.com/questions/25797353/hunchentoot-based-app-in-a-lisp-image-from-buildapp-immediately-returns
+    #+sbcl(sb-impl::toplevel-repl nil)))
 
 (setf (documentation #'serve 'function)
       (documentation *package* t))
