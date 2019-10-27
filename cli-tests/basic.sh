0e35b72fc29ecd8f716027506d16a7c875b5575d
Masataro Asai <guicho2.71828@gmail.com>
2019-10-27 13:18:04 -0400

    [travis] trying IPv4 to test the preview feature

--- cli-tests/basic.sh
+++ cli-tests/basic.sh
@@ -25,9 +25,9 @@ trap "kill $pid; rm -rf $dir" EXIT
 
 sleep 3
 
-wget 127.0.0.1:5000 -O-
+wget -4 -O- 127.0.0.1:5000
 
-! wget 127.0.0.1:5000/nosuchurl -O-
+! wget -4 -O- 127.0.0.1:5000/nosuchurl
 
 # (
 #     wget 127.0.0.1:5000/nosuchurl -O-
