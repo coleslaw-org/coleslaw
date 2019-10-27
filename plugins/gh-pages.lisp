beb627c482128c4604ae4c9906d4fbadf12f9296
Masataro Asai <guicho2.71828@gmail.com>
2019-10-27 18:01:02 -0400

    a simple approach using shell commands

--- plugins/gh-pages.lisp
+++ plugins/gh-pages.lisp
@@ -1,6 +1,38 @@
+(eval-when (:compile-toplevel :load-toplevel)
+  (ql:quickload 'puri :silent t))
 (defpackage :coleslaw-gh-pages
   (:use :cl)
+  (:import-from :coleslaw
+                #:*config*
+                #:domain
+                #:deploy
+                #:staging-dir
+                #:deploy-dir)
   (:export #:enable))
 
 (in-package :coleslaw-gh-pages)
 
+(defvar *options* nil)
+
+(defmethod deploy (staging)
+  (uiop:run-program (list* (namestring
+                            (merge-pathnames "plugins/publish-gh-pages.sh"
+                                             coleslaw-conf:*basedir*))
+                           (namestring
+                            (merge-pathnames (staging-dir *config*)))
+                           (namestring
+                            (merge-pathnames (deploy-dir *config*)))
+                           *options*)
+                    :output t
+                    :error-output t))
+
+(defun enable (&key url (branch "gh-pages") (remote "origin") cname)
+  (check-type url string)
+  (check-type remote string)
+  (check-type branch string)
+  (if (eq t cname)
+      (progn
+        (setf cname (puri:uri-host (puri:parse-uri (domain *config*))))
+        (check-type cname string)
+        (setf *options* (list url branch remote cname)))
+      (setf *options* (list url branch remote))))
