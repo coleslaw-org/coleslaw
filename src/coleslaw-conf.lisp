5fd748be8b1b90d7d9a4201442f942d0d0414661
Masataro Asai <guicho2.71828@gmail.com>
2019-10-27 15:36:20 -0400

    *load-truename* does not work, it may point to the cached fasl file.

--- src/coleslaw-conf.lisp
+++ src/coleslaw-conf.lisp
@@ -1,10 +1,10 @@
 (defpackage #:coleslaw-conf
   (:use #:cl)
-  (:import-from #:uiop/pathname
-                #:pathname-parent-directory-pathname)
   (:export #:*basedir*))
 
 (in-package #:coleslaw-conf)
 
-(defparameter *basedir* (pathname-parent-directory-pathname (make-pathname :name nil :type nil :defaults *load-truename*))
+(defparameter *basedir*
+  (uiop/pathname:pathname-parent-directory-pathname
+   #.(or *compile-file-truename* *load-truename*))
   "A pathname pointing to Coleslaw's top level directory.")
