a9de710b0f356a72d21970ba2c28c4a241a1c8f0
Cthulhux <git@tuxproject.de>
2024-05-12 22:14:51 +0200

    Update and rename piwik.lisp to matomo.lisp

--- plugins/matomo.lisp
+++ plugins/matomo.lisp
@@ -1,11 +1,11 @@
-(defpackage :coleslaw-piwik
+(defpackage :coleslaw-matomo
   (:use :cl)
   (:export #:enable)
   (:import-from :coleslaw #:add-injection))
 
-(in-package :coleslaw-piwik)
+(in-package :coleslaw-matomo)
 
-(defvar *piwik-js*
+(defvar *matomo-js*
   "<script type="text/javascript">
   var _paq = _paq || [];
   _paq.push(['trackPageView']);
@@ -13,13 +13,13 @@
 
   (function() {
     var u='//~a/';
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '~a']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>")
 
-(defun enable (&key piwik-url piwik-site)
-  (let ((snippet (format nil *piwik-js* piwik-url piwik-site)))
+(defun enable (&key matomo-url matomo-site)
+  (let ((snippet (format nil *matomo-js* matomo-url matomo-site)))
     (add-injection (constantly snippet) :head)))
